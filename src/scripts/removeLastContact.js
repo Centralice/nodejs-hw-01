731418c17f37e4188f12e7ce3615d2ad0f38502b
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-17 20:57:35 +0200

    upd

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -3,10 +3,12 @@ import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeLastContact = async () => {
   const contacts = await readContacts();
-  if (contacts.length) {
-    contacts.length = contacts.length - 1;
-    await writeContacts(contacts);
-  }
+  // if (contacts.length) {
+  //   contacts.length = contacts.length - 1;
+  //   await writeContacts(contacts);
+  // }
+  contacts.pop();
+  await writeContacts(contacts);
 };
 
 removeLastContact();
